9d7c6d814a9e6113376f9f900279037a31f6d1a1
SUNNY01BHATIA <sunnykumarbhatia9@gmail.com>
2024-12-19 22:17:54 +0530

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,35 +1,3 @@
-class SimpleCalculator:
-    @staticmethod
-    def add(a, b):
-        return a + b
+import steamlit as st
 
-    @staticmethod
-    def subtract(a, b):
-        return a - b
-
-    @staticmethod
-    def multiply(a, b):
-        return a * b
-
-    @staticmethod
-    def divide(a, b):
-        if b != 0:
-            return a / b
-        else:
-            return "Error: Division by zero"
-
-# Define two numbers
-num1 = 10
-num2 = 5
-
-# Perform calculations
-addition_result = SimpleCalculator.add(num1, num2)
-subtraction_result = SimpleCalculator.subtract(num1, num2)
-multiplication_result = SimpleCalculator.multiply(num1, num2)
-division_result = SimpleCalculator.divide(num1, num2)
-
-# Display results
-print(f"Addition: {num1} + {num2} = {addition_result}")
-print(f"Subtraction: {num1} - {num2} = {subtraction_result}")
-print(f"Multiplication: {num1} * {num2} = {multiplication_result}")
-print(f"Division: {num1} / {num2} = {division_result}")
+st.write('Hello World')
